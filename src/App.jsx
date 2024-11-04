bcd5dfb9254156d2b3ccbb0e9596d8762baa3f5b
Nirmalya Mandal <149013522+DevNiru2704@users.noreply.github.com>
2024-11-04 08:22:22 +0530

    Set Default route to /home

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Navbar from './components/Navbar.jsx';
 import Home from './pages/Home.jsx';
 import TutorRegistration from './pages/TutorRegistration.jsx';
@@ -14,12 +14,13 @@ function App() {
       <div className="min-h-screen bg-gray-50">
         <Navbar />
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
           <Route path="/tutor-registration" element={<TutorRegistration />} />
           <Route path="/student-registration" element={<StudentRegistration />} />
           <Route path="/tutor/:id" element={<TutorProfile />} />
           <Route path="/login" element={<Login />} />
           <Route path="/search" element={<SearchResults />} />
+          <Route path="/*" element={<Navigate to="/home" />}/>
         </Routes>
       </div>
     </Router>
